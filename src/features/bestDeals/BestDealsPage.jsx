a1f77dbf8f68e0701b542c586399be58ddd2f16b
unknown <ama991969@gmail.com>
2025-02-23 02:20:05 +0200

    Updata Project

--- src/features/bestDeals/BestDealsPage.jsx
+++ src/features/bestDeals/BestDealsPage.jsx
@@ -1,4 +1,4 @@
-import ProductCard from "../../components/products/ProductCard";
+import ProductCard from "../../component/products/ProductCard";
 
 import { useTranslation } from "react-i18next";
 
