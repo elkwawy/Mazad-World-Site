a4469bb57bf2016b59b3dc58f99d05a3d5a646da
Ashraf Qopiah <124946844+ashrafmo-1@users.noreply.github.com>
2025-03-10 00:29:07 +0200

    init deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: [".js", ".jsx"],
     alias: {
-      "@": path.resolve(__dirname, "src"), // "src" تعريف "@" كاختصار لمسار
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "src"),
     },
+
+    base: "mazad-world.masar-soft.com",
   },
 })
